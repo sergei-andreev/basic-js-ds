880ab498c5c909b5e4cea4874f93ee68b8f9b7e0
Sergei Andreev <sergey.andreev@dualbootpartners.com>
2023-04-14 23:49:42 +0200

    feat: add solution for stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    stack = [];
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    push(element) {
+        this.stack.push(element);
+    }
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    pop() {
+        return this.stack.pop();
+    }
+
+    peek() {
+        return this.stack.slice(-1)[0];
+    }
 }
 
 module.exports = {
-  Stack
+    Stack
 };
